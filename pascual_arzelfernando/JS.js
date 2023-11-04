5e54ec20f397b4a6bb3b22206bc5441f477f7fc5
Arzel Fernando Pascual <afPascual@gmail.com>
2023-11-05 04:09:39 +0800

    modify files

--- pascual_arzelfernando/JS.js
+++
@@ -1,9 +0,0 @@
-function onTextChange() {
-    let userName = document.getElementById("name");
-    let userComment = document.getElementById("comment");
-    if (userName.value.length > 0 && userComment.value.length > 0){ 
-            document.getElementById("submit").disabled = false;
-    } 
-    else {
-    document.getElementById("submit").disabled = true;
-    }}
