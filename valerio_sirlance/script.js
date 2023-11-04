96f626bc75a126f4140d67d07b9ec4e52e41fed3
SLValerio <vsirlance@gmail.com>
2023-11-04 22:25:39 +0800

    add valerio_sirlance files

---
+++ valerio_sirlance/script.js
@@ -0,0 +1,15 @@
+function commentButton() {
+    let userName = document.getElementById("user_name");
+    let userComment = document.getElementById("user_comment");
+    let commentButton = document.getElementById("comment_button");
+    if(userName.value.length > 0 && userComment.value.length > 0) {
+        document.getElementById("comment_button").disabled = false;
+    }
+    else {
+        document.getElementById("comment_button").disabled = true;
+    }
+    commentButton.addEventListener("click", function() {
+        alert("Comment sent successfully!");
+    });
+    
+}
