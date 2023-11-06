71ca4796f729f8d49362dbd0156c93da0fde8001
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2023-11-06 10:22:38 +0800

    fix comment section

--- monroy_jeffcarl/monroy_jeffcarl.js
+++ monroy_jeffcarl/monroy_jeffcarl.js
@@ -1,10 +1,45 @@
-//commentbutton
-function commentButton() {
-    let userInput = document.getElementById("user_input");
-    let userComment = document.getElementById("comment_input");
-    if(userInput.value.length > 0 && userComment.value.length > 0) {
-      document.getElementById("comment_button").disabled = false;
-    } else {
-      document.getElementById("comment_button").disabled = true;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  let addButton = document.querySelector("#add_button");
+  let fullNameInput = document.querySelector("#full_name");
+  let commentInput = document.querySelector("#comment");
+
+  addButton.addEventListener("click", addComment);
+
+  fullNameInput.addEventListener("input", enableCommentButton);
+  commentInput.addEventListener("input", enableCommentButton);
+});
+
+function enableCommentButton() {
+  let fullNameInput = document.querySelector("#full_name");
+  let commentInput = document.querySelector("#comment");
+  let addButton = document.querySelector("#add_button");
+
+  addButton.disabled = fullNameInput.value.trim() === "" ||
+  commentInput.value.trim() === "";
 }
+
+function addComment() {
+  let fullNameInput = document.querySelector("#full_name");
+  let commentInput = document.querySelector("#comment");
+  let itemList = document.querySelector("#comment_list");
+
+  let fullName = fullNameInput.value;
+  let comment = commentInput.value;
+
+  let listItem = document.createElement("li");
+  listItem.textContent = fullName; 
+  let commentText = document.createElement("p"); 
+  commentText.textContent = comment;
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function () {
+    itemList.removeChild(listItem);
+  });
+  listItem.appendChild(commentText);
+  listItem.appendChild(deleteButton);
+  itemList.appendChild(listItem);
+  fullNameInput.value = "";
+  commentInput.value = "";
+  enableCommentButton();
+}
+
